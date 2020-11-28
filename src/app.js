3e52d3c04803b59db9e5c1c56512e9f1416a6b66
Acyclicstudent <axel_07_avila@hotmail.com>
2020-11-28 02:30:43 -0600

    Puerto y package

--- src/app.js
+++ src/app.js
@@ -158,7 +158,7 @@ app.post('/notifications', (req, res) => {
     res.sendStatus(200);
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server on port 3000');
 });
 
